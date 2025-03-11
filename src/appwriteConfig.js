423dfced73e386d9dfec6110147e1ccfb4224af2
dhanush77777 <saidhanushm@gmail.com>
2025-03-11 17:14:44 +0530

    .

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,12 +1,10 @@
 import { Client, Databases } from "appwrite";
 
-// 🔥 Initialize Appwrite Client
 const client = new Client();
 client
-  .setEndpoint("https://cloud.appwrite.io/v1") // Replace with your Appwrite endpoint
-  .setProject("67cff25f00040f6cf6c2"); // Replace with your project ID
+  .setEndpoint("https://cloud.appwrite.io/v1") 
+  .setProject("67cff25f00040f6cf6c2"); 
 
-// 🔥 Initialize Database
 const databases = new Databases(client);
 
 export { client, databases };
