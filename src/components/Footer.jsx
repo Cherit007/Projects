6b927f262059b4496d1146afa3c06d82ca8ffc53
Cherit <cherit@Cherits-MacBook-Air.local>
2025-03-19 23:00:27 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,63 +2,52 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div
-      className="container-fluid bg-dark text-light footer mt-5 pt-5 wow fadeIn"
-      data-wow-delay="0.1s"
-    >
-      <div className="footer-container">
-        <div className="col-lg-3 col-md-6">
-          <h4 className="text-light mb-4">Address</h4>
-          <p className="mb-2">
-            <i className="fa fa-map-marker-alt me-3"></i>Madhya Pradesh, India
-          </p>
-          <p className="mb-2">
-            <i className="fa fa-phone-alt me-3"></i>+91 91110 97070
-          </p>
-          {/* <p className="mb-2">
-              <i className="fa fa-envelope me-3"></i>info@example.com
-            </p> */}
-          <div className="d-flex pt-2">
-            <a
-              target="_blank"
-              className="btn btn-outline-light btn-social"
-              href="https://www.facebook.com/SajjanVermaOfficial/about_profile_transparency"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a
-              target="_blank"
-              className="btn btn-outline-light btn-social"
-              href="https://x.com/sajjanvermainc?lang=en"
-            >
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            {/* <a className="btn btn-outline-light btn-social" href="">
-                <i className="fab fa-youtube"></i>
-              </a>
-              <a className="btn btn-outline-light btn-social" href="">
-                <i className="fab fa-linkedin-in"></i>
-              </a> */}
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6">
-          <h4 className="text-light mb-4">Quick Links</h4>
-          <a className="btn btn-link" href="/about">
-            About Us
+    <div className="footer-container">
+      <div className="footer-column">
+        <h4 className="footer-heading">Address</h4>
+        <p>
+          <i className="fa fa-map-marker-alt icon"></i>Madhya Pradesh, India
+        </p>
+        <p>
+          <i className="fa fa-phone-alt icon"></i>+91 91110 97070
+        </p>
+        <div className="social-icons">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-button"
+            href="https://www.facebook.com/SajjanVermaOfficial/about_profile_transparency"
+          >
+            <i className="fab fa-twitter"></i>
           </a>
-          <a className="btn btn-link" href="/contactus">
-            Contact Us
-          </a>
-          <a className="btn btn-link" href="/donate">
-            Donations
-          </a>
-          <a className="btn btn-link" href="/events">
-            Events
-          </a>
-          <a className="btn btn-link" href="/coremembers">
-            Core Members
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-button"
+            href="https://x.com/sajjanvermainc?lang=en"
+          >
+            <i className="fab fa-facebook-f"></i>
           </a>
         </div>
+      </div>
+
+      <div className="footer-column">
+        <h4 className="footer-heading">Quick Links</h4>
+        <a className="footer-link" href="/about">
+          About Us
+        </a>
+        <a className="footer-link" href="/contactus">
+          Contact Us
+        </a>
+        <a className="footer-link" href="/donate">
+          Donations
+        </a>
+        <a className="footer-link" href="/events">
+          Events
+        </a>
+        <a className="footer-link" href="/coremembers">
+          Core Members
+        </a>
       </div>
     </div>
   );
