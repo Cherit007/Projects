bbbc4f0dd84e055e912383603692418e641401ba
kailash0105 <kailash.muthyam@gmail.com>
2025-03-21 18:50:20 +0530

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,40 +10,15 @@ import {
 } from "lucide-react";
 import "../index.css";
 import Navbar from "./Navbar";
+import Heading from "./LogosHeading";
 
 const Header = () => {
   return (
     <div>
-      <div className="header-container">
-        <div className="header-content">
-          <div className="left-section">
-            <div className="info-item">
-              <MapPin className="icon primary-icon" />
-              <small>मध्य प्रदेश</small>
-            </div>
-            <div className="info-item">
-              <Clock className="icon primary-icon" />
-              <small>Mon - Fri : 09.00 AM - 09.00 PM</small>
-            </div>
-          </div>
-          <div className="right-section">
-            <div className="info-item">
-              <Phone className="icon primary-icon" />
-              <small>+91 91110 97070</small>
-            </div>
-            <div className="social-icons">
-              <a target="_blank" className="social-icon-link" href="https://www.facebook.com/SajjanVermaOfficial/about_profile_transparency">
-                <Facebook className="icon" />
-              </a>
-              <a target="_blank" className="social-icon-link" href="https://x.com/sajjanvermainc?lang=en">
-                <Twitter className="icon" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+    
 
-      <div id="google_translate_element" className="translate-widget"></div>
+       <div id="google_translate_element" className="translate-widget"></div>
+      <Heading />
       <Navbar />
     </div>
   );
